e2db7d5eb0480708a9cd8a71aa5f1b6fbce1995d
Angelina <ZolotovaAngelina02@yandex.ru>
2025-02-25 17:38:57 +0300

    add icons

--- script.js
+++ script.js
@@ -5,10 +5,9 @@ document.getElementById('change-theme').addEventListener('click', function() {
 
 // Карусель изображений
 const images = document.querySelectorAll('.carousel img');
-let currentIndex = 0;
 const dots = document.querySelectorAll('.carousel-dots .dot');
+let currentIndex = 0;
 
-// Обновление карусели
 function updateCarousel(index) {
     images.forEach(img => img.classList.remove('active'));
     dots.forEach(dot => dot.classList.remove('active'));
@@ -16,11 +15,19 @@ function updateCarousel(index) {
     dots[index].classList.add('active');
 }
 
+// Слушатели кликов на точки
+dots.forEach(dot => {
+    dot.addEventListener('click', function() {
+        currentIndex = parseInt(this.dataset.index);
+        updateCarousel(currentIndex);
+    });
+});
+
 // Переключение изображений при клике на левую или правую часть
 function changeImage(event) {
     const totalImages = images.length;
     const carouselWidth = event.target.offsetWidth;
-    
+
     if (event.offsetX < carouselWidth / 2) {
         // Клик в левую часть
         currentIndex = (currentIndex - 1 + totalImages) % totalImages;
@@ -32,16 +39,6 @@ function changeImage(event) {
     updateCarousel(currentIndex);
 }
 
-// Добавляем обработчик для клика по карусели
 document.querySelector('.carousel').addEventListener('click', changeImage);
 
-// Обработчик для переключения по точкам
-dots.forEach(dot => {
-    dot.addEventListener('click', function() {
-        currentIndex = parseInt(this.dataset.index);
-        updateCarousel(currentIndex);
-    });
-});
-
-// Инициализация карусели с начальным изображением
 updateCarousel(currentIndex);
